2e4d80c04089f93a1aba00ec5a09bea0b810c3b5
Israel316 <isra_arti@hotmail.com>
2021-01-08 19:22:54 -0500

    WS01 Hello World in three programming languages

--- workshops/Hello World/hola mundo.c
+++ workshops/Hello World/hola mundo.c
@@ -3,5 +3,5 @@ int main(){
 	int grade
 	//printf() displays information
 	printf("Hello World from \t \" Santiago Arteaga! \" =) \n");
-	printf ("\n The grade is %d \ n", garde);
+	printf ("\n The grade is %d \n", grAde);
 	return 0;
