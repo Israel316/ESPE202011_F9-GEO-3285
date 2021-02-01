22daddff20aabdd516a1fd49fe1e2f17775b850c
Israel316 <isra_arti@hotmail.com>
2021-01-31 20:50:48 -0500

    HW08-FactorialNumber

--- Assignments/HW08-FactorialNumber/FactorialNumber/src/factorialnumber/FactorialNumber.java
+++ Assignments/HW08-FactorialNumber/FactorialNumber/src/factorialnumber/FactorialNumber.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package factorialnumber;
-
+import java.util.Scanner;
 /**
  *
  * @author HP
@@ -16,6 +16,34 @@ public class FactorialNumber {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+  int factorial= 1;
+        int number = 0;
+        int stop=15;
+        int product =0;
+        
+                do{
+            
+         System.out.println("ENTER A NUMBER EQUAL TO FACTORIAL: ");
+         System.out.println("NEGATIVE NUMBERS to exit  --> ");
+         System.out.println("NUMBER GREATER THAN 15 EQUALS ERROR--> to exit  ");
+         
+         Scanner input=new Scanner (System.in);
+         number=input.nextInt();
+         
+         factorial=1;
+         while(number !=0)
+         {
+             factorial *=number;
+              number --;       
+         }
+        System.out.println("the factorial number is "  +  factorial);
+        
+    }while (factorial !=0);
+    number  = -0;
+    System.out.println("good bye");
+} 
+
 }
+    
+    
+
